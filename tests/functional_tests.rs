acce2aa1b14cc5b3dd8827f03afb8d37f616f368
Cade Jacobson <cadejjacobson@gmail.com>
2023-06-22 21:13:19 +0000

    Create initial functional tests

--- tests/functional_tests.rs
+++ tests/functional_tests.rs
@@ -2,9 +2,14 @@ use tokio;
 
 use lib::{goalstate, imds, user};
 use lib::distro::{Distribution, Distributions};
+use lib::imds::PublicKeys;
+
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let cli_args: Vec<String> = env::args().collect();
+
     let get_goalstate_result = goalstate::get_goalstate().await;
     let vm_goalstate = match get_goalstate_result {
         Ok(vm_goalstate) => vm_goalstate,
@@ -17,17 +22,7 @@ async fn main() {
         Err(_err) => return,
     };
 
-    let query_result = imds::query_imds().await;
-    let imds_body = match query_result {
-        Ok(imds_body) => imds_body,
-        Err(_err) => return,
-    };
-
-    let username = imds::get_username(imds_body.clone());
-    let username = match username {
-        Ok(username) => username,
-        Err(_err) => return,
-    };
+    let username = &cli_args[1];
 
     let mut file_path = "/home/".to_string();
     file_path.push_str(username.as_str());
@@ -36,21 +31,13 @@ async fn main() {
     let _create_directory =
         user::create_ssh_directory(username.as_str(), file_path.clone()).await;
 
-    let get_ssh_key_result = imds::get_ssh_keys(imds_body.clone());
-    let keys = match get_ssh_key_result {
-        Ok(keys) => keys,
-        Err(_err) => return,
-    };
+    let mut keys:Vec<PublicKeys> = Vec::new();
+    keys.push(PublicKeys{path: "/path/to/.ssh/keys/".to_owned(), key_data: "ssh-rsa test_key_1".to_owned()});
+    keys.push(PublicKeys{path: "/path/to/.ssh/keys/".to_owned(), key_data: "ssh-rsa test_key_2".to_owned()});
+    keys.push(PublicKeys{path: "/path/to/.ssh/keys/".to_owned(), key_data: "ssh-rsa test_key_3".to_owned()});
 
     file_path.push_str("/.ssh");
 
     user::set_ssh_keys(keys, username.to_string(), file_path.clone()).await;
-
-    let get_hostname_result = imds::get_hostname(imds_body.clone());
-    let hostname = match get_hostname_result {
-        Ok(hostname) => hostname,
-        Err(_err) => return,
-    };
-
-    Distributions::from("ubuntu").set_hostname(hostname.as_str()).expect("Failed to set hostname");
+    Distributions::from("ubuntu").set_hostname("test-hostname-set").expect("Failed to set hostname");
 }
