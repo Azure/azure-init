d98c029af4849a588dec7104dc2d99f82eafaa2f
Cade Jacobson <cadejjacobson@gmail.com>
2023-06-23 17:54:04 +0000

    Fix makefile's call location

--- tests/functional_tests.rs
+++ tests/functional_tests.rs
@@ -1,6 +1,6 @@
 use tokio;
 
-use lib::{goalstate, imds, user};
+use lib::{goalstate, user};
 use lib::distro::{Distribution, Distributions};
 use lib::imds::PublicKeys;
 
@@ -10,11 +10,22 @@ use std::env;
 async fn main() {
     let cli_args: Vec<String> = env::args().collect();
 
+    println!("**********************************");
+    println!("* Beginning functional testing");
+    println!("**********************************");
+    println!("");
+
+    println!("Querying wireserver for Goalstate");
+
     let get_goalstate_result = goalstate::get_goalstate().await;
     let vm_goalstate = match get_goalstate_result {
         Ok(vm_goalstate) => vm_goalstate,
         Err(_err) => return,
     };
+
+    println!("Goalstate successfully received");
+    println!("");
+    println!("Reporting VM Health to wireserver");
 
     let report_health_result = goalstate::report_health(vm_goalstate).await;
     let _report_health = match report_health_result {
@@ -22,14 +33,30 @@ async fn main() {
         Err(_err) => return,
     };
 
+    println!("VM Health successfully reported");
+
     let username = &cli_args[1];
 
     let mut file_path = "/home/".to_string();
     file_path.push_str(username.as_str());
 
+    println!("");
+    println!("Attempting to create user {}", username.as_str());
+
     Distributions::from("ubuntu").create_user(username.as_str()).expect("Failed to create user");
+
+    println!("User {} was successfully created", username.as_str());
+
+    println!("");
+    println!("Attempting to create user's SSH directory");
+
     let _create_directory =
         user::create_ssh_directory(username.as_str(), file_path.clone()).await;
+    let _create_directory = match _create_directory {
+        Ok(create_directory) => create_directory,
+        Err(_err) => return,
+    };
+    println!("User's SSH directory was successfully created");
 
     let mut keys:Vec<PublicKeys> = Vec::new();
     keys.push(PublicKeys{path: "/path/to/.ssh/keys/".to_owned(), key_data: "ssh-rsa test_key_1".to_owned()});
@@ -39,5 +66,11 @@ async fn main() {
     file_path.push_str("/.ssh");
 
     user::set_ssh_keys(keys, username.to_string(), file_path.clone()).await;
+
+    println!("");
+    println!("Attempting to set the VM hostname");
+
     Distributions::from("ubuntu").set_hostname("test-hostname-set").expect("Failed to set hostname");
+    println!("VM hostname successfully set");
+    println!("");
 }
