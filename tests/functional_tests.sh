e2aa4c2a583aa6d4e5b14412bb637315c05514cc
Cade Jacobson <cadejjacobson@gmail.com>
2023-06-28 18:13:01 +0000

    Ensure script exits on failure

--- tests/functional_tests.sh
+++ tests/functional_tests.sh
@@ -5,16 +5,15 @@ LOCATION=eastus
 PATH_TO_PUBLIC_SSH_KEY="$HOME/.ssh/id_rsa.pub"
 PATH_TO_PRIVATE_SSH_KEY="$HOME/.ssh/id_rsa"
 VM_NAME="AzProvAgentFunctionalTest"
-VM_IMAGE="Ubuntu2204"
+VM_IMAGE="Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest"
 VM_SIZE="Standard_D2lds_v5"
 VM_ADMIN_USERNAME="azureuser"
 AZURE_SSH_KEY_NAME="azure-ssh-key"
 VM_NAME_WITH_TIMESTAMP=$VM_NAME-$EPOCH
 
-echo "Starting script"
-echo "Logging you into Azure"
+set -e
 
-# TODO: Add checking for Az CLI
+echo "Starting script"
 
 if [ ! -f "$PATH_TO_PUBLIC_SSH_KEY" ]; then
     ssh-keygen -t rsa -b 4096 -f $PATH_TO_PUBLIC_SSH_KEY -N ""
@@ -24,7 +23,11 @@ else
 fi
 
 # Log into Azure (this will open a browser window prompting you to log in)
+echo "Logging you into Azure"
 az login
+
+# Ensure Azure is logged in
+az account get-access-token -o none
 
 # Set the subscription you want to use
 az account set --subscription $SUBSCRIPTION_ID
@@ -40,6 +43,10 @@ az vm create -n $VM_NAME_WITH_TIMESTAMP \
 --admin-username $VM_ADMIN_USERNAME \
 --ssh-key-value $PATH_TO_PUBLIC_SSH_KEY \
 --public-ip-sku Standard
+echo "VM successfully created"
+
+echo "Sleeping to ensure SSH access set up"
+sleep 5
 
 echo "Getting VM Public IP Address..."
 PUBLIC_IP=$(az vm show -d -g $RG -n $VM_NAME_WITH_TIMESTAMP --query publicIps -o tsv)
@@ -47,7 +54,6 @@ echo $PUBLIC_IP
 
 scp -o StrictHostKeyChecking=no -i $PATH_TO_PRIVATE_SSH_KEY ./target/debug/functional_tests $VM_ADMIN_USERNAME@$PUBLIC_IP:~
 
-# TODO: Handle SSH Failure (need to abort script)
 echo "Logging into VM..."
 ssh -o StrictHostKeyChecking=no -i $PATH_TO_PRIVATE_SSH_KEY $VM_ADMIN_USERNAME@$PUBLIC_IP 'sudo ./functional_tests test_user' 
 
