a4fa05754ca9b3fde17a368857a24222ae557d9d
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-01-23 12:53:35 -0500

    Standardize logging when running subprocesses (#155)
    This adds a helper function which accepts a Command to run and has
    uniform logging when running a command.
    
    The command is logged at the debug level prior to execution, and
    afterwards the status is also logged at debug level. If the command
    failed, it's logged at error level and includes the command's stdout and
    stderr.
    
    This also ensures all commands run used piped stdio rather than
    inheriting the parent. Allowing the child to write to the parent
    stdout/stderr leads to unstructured command output mixed into the stderr
    log output. The easiest way to do this is to use `output()` rather than
    `status()` when running the command.

--- libazureinit/src/lib.rs
+++ libazureinit/src/lib.rs
@@ -16,3 +16,46 @@ mod unittest;
 
 // Re-export as the Client is used in our API.
 pub use reqwest;
+
+/// Run a command, capturing its output and logging it if it fails.
+///
+/// In the event of a failure, the provided `error_message` is logged at
+/// error level.
+///
+/// <div class="warning">
+///
+/// This logs the command and its arguments, and as such is not appropriate
+/// if the command contains sensitive information.
+///
+/// </div>
+pub(crate) fn run(
+    mut command: std::process::Command,
+) -> Result<(), error::Error> {
+    let program = command.get_program().to_string_lossy().to_string();
+    let span = tracing::info_span!("subprocess", program);
+    let _entered = span.enter();
+
+    tracing::debug!(?command, "About to execute system program");
+    let output = command.output()?;
+    let status = output.status;
+    tracing::debug!(?status, "System program completed");
+
+    if !status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        tracing::error!(
+            ?status,
+            ?command,
+            ?stdout,
+            ?stderr,
+            "Command '{}' failed",
+            program
+        );
+        return Err(error::Error::SubprocessFailed {
+            command: format!("{:?}", command),
+            status,
+        });
+    }
+
+    Ok(())
+}
