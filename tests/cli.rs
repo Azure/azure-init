3ff4a0a3b455ba2a0b8a0282f8c04805e4651261
Cade Jacobson <cadejacobson@microsoft.com>
2025-07-01 13:56:34 -0700

    Split common code into a separate function

--- tests/cli.rs
+++ tests/cli.rs
@@ -20,9 +20,16 @@ fn help_groups() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[test]
-fn clean_removes_only_provision_files_without_log_arg(
-) -> Result<(), Box<dyn std::error::Error>> {
+// Helper function to set up the log and provision files for cleaning
+fn setup_clean_test() -> Result<
+    (
+        tempfile::TempDir,
+        std::path::PathBuf,
+        std::path::PathBuf,
+        std::path::PathBuf,
+    ),
+    Box<dyn std::error::Error>,
+> {
     let temp_dir = tempdir()?;
     let data_dir = temp_dir.path().join("data");
     let log_file = temp_dir.path().join("azure-init.log");
@@ -47,6 +54,16 @@ fn clean_removes_only_provision_files_without_log_arg(
     );
     let config_path = temp_dir.path().join("azure-init-config.toml");
     fs::write(&config_path, config_contents)?;
+
+    Ok((temp_dir, data_dir, log_file, config_path))
+}
+
+// Ensures that the `clean` command removes only the provisioned file
+#[test]
+fn clean_removes_only_provision_files_without_log_arg(
+) -> Result<(), Box<dyn std::error::Error>> {
+    let (_temp_dir, _data_dir, log_file, config_path) = setup_clean_test()?;
+    let provisioned_file = _data_dir.join("vm-id.provisioned");
 
     assert!(
         provisioned_file.exists(),
@@ -68,33 +85,13 @@ fn clean_removes_only_provision_files_without_log_arg(
     Ok(())
 }
 
+// Ensures that the `clean` command with the --logs arg
+// removes both the provisioned file and the log file
 #[test]
 fn clean_removes_provision_and_log_files_with_log_arg(
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let temp_dir = tempdir()?;
-    let data_dir = temp_dir.path().join("data");
-    let log_file = temp_dir.path().join("azure-init.log");
-    fs::create_dir_all(&data_dir)?;
-
-    let provisioned_file = data_dir.join("vm-id.provisioned");
-    File::create(&provisioned_file)?;
-
-    let mut log = File::create(&log_file)?;
-    writeln!(log, "fake log line")?;
-
-    let config_contents = format!(
-        r#"
-        [azure_init_data_dir]
-        path = "{}"
-
-        [azure_init_log_path]
-        path = "{}"
-        "#,
-        data_dir.display(),
-        log_file.display()
-    );
-    let config_path = temp_dir.path().join("azure-init-config.toml");
-    fs::write(&config_path, config_contents)?;
+    let (_temp_dir, _data_dir, log_file, config_path) = setup_clean_test()?;
+    let provisioned_file = _data_dir.join("vm-id.provisioned");
 
     assert!(
         provisioned_file.exists(),
