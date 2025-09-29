c8f09a3fb1242986351daad2656d5fdce75898cf
Cade Jacobson <cadejacobson@microsoft.com>
2025-09-29 18:04:29 +0000

    Split test server into multiple files

---
+++ testinit/testing-server/imds_handler.py
@@ -0,0 +1,167 @@
+from http.server import BaseHTTPRequestHandler
+import json
+from urllib.parse import urlparse, parse_qs
+
+from config import IMDS_GET_DELAY, IMDS_GET_TIMEOUT
+from utils import logger
+
+
+# Mock metadata responses
+MOCK_INSTANCE_METADATA = {
+    "compute": {
+        "azEnvironment": "AzurePublicCloud",
+        "customData": "",
+        "isHostCompatibilityLayerVm": "false",
+        "licenseType": "",
+        "location": "eastus",
+        "name": "test-vm",
+        "offer": "UbuntuServer",
+        "osProfile": {
+            "adminUsername": "userforazure",
+            "computerName": "test-vm",
+            "disablePasswordAuthentication": "true"
+        },
+        "osType": "Linux",
+        "placementGroupId": "",
+        "plan": {
+            "name": "",
+            "product": "",
+            "publisher": ""
+        },
+        "platformFaultDomain": "0",
+        "platformUpdateDomain": "0",
+        "provider": "Microsoft.Compute",
+        "publicKeys": [],
+        "publisher": "Canonical",
+        "resourceGroupName": "test-rg",
+        "resourceId": "/subscriptions/12345678-90ab-cdef-1234-567890abcdef/resourceGroups/test-rg/providers/Microsoft.Compute/virtualMachines/test-vm",
+        "securityProfile": {
+            "secureBootEnabled": "false",
+            "virtualTpmEnabled": "false"
+        },
+        "sku": "18.04-LTS",
+        "storageProfile": {
+            "dataDisks": [],
+            "imageReference": {
+                "offer": "UbuntuServer",
+                "publisher": "Canonical",
+                "sku": "18.04-LTS",
+                "version": "latest"
+            },
+            "osDisk": {
+                "caching": "ReadWrite",
+                "createOption": "FromImage",
+                "diskSizeGB": "30",
+                "image": {
+                    "uri": ""
+                },
+                "managedDisk": {
+                    "id": "/subscriptions/12345678-90ab-cdef-1234-567890abcdef/resourceGroups/test-rg/providers/Microsoft.Compute/disks/test-vm_OsDisk_1",
+                    "storageAccountType": "Premium_LRS"
+                },
+                "name": "test-vm_OsDisk_1",
+                "osType": "Linux",
+                "vhd": {
+                    "uri": ""
+                },
+                "writeAcceleratorEnabled": "false"
+            }
+        },
+        "subscriptionId": "12345678-90ab-cdef-1234-567890abcdef",
+        "tags": "Environment:Test;Project:ProvisioningAgent",
+        "version": "18.04.202109080",
+        "vmId": "12345678-90ab-cdef-1234-567890abcdef",
+        "vmScaleSetName": "",
+        "vmSize": "Standard_B2s",
+        "zone": "1"
+    },
+    "network": {
+        "interface": [
+            {
+                "ipv4": {
+                    "ipAddress": [
+                        {
+                            "privateIpAddress": "10.0.0.4",
+                            "publicIpAddress": "52.168.1.1"
+                        }
+                    ],
+                    "subnet": [
+                        {
+                            "address": "10.0.0.0",
+                            "prefix": "24"
+                        }
+                    ]
+                },
+                "ipv6": {
+                    "ipAddress": []
+                },
+                "macAddress": "000D3A123456"
+            }
+        ]
+    }
+}
+
+class IMDSHandler(BaseHTTPRequestHandler):
+    """HTTP handler for Azure Instance Metadata Service requests."""
+    self.imds_get_delay = IMDS_GET_DELAY
+    self.imds_get_timeout = IMDS_GET_TIMEOUT
+    
+    def log_message(self, format, *args):
+        """Override to use our logger."""
+        logger.info(f"IMDS: {format % args}")
+    
+    def do_GET(self):
+        """Handle GET requests to IMDS endpoints."""
+        parsed_url = urlparse(self.path)
+        query_params = parse_qs(parsed_url.query)
+        
+        logger.info(f"IMDS GET request: {self.path}")
+        logger.info(f"Headers: {dict(self.headers)}")
+        
+        if self.imds_get_timeout:
+            logger.info(f"Adding IDMS GET timeout from ENV variable")
+            return
+
+        if self.imds_get_delay != 0:
+            logger.info(f"Adding IMDS GET request delay of {self.imds_get_delay} seconds")
+            time.sleep(self.imds_get_delay)
+
+        # Check for required Metadata header
+        if self.headers.get('Metadata') != 'true':
+            self.send_response(400)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            error_response = {"error": "Bad Request", "message": "Metadata header not found"}
+            self.wfile.write(json.dumps(error_response).encode())
+            return
+        
+        # Handle different IMDS endpoints
+        if parsed_url.path == '/metadata/instance':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(MOCK_INSTANCE_METADATA).encode())
+        
+        elif parsed_url.path == '/metadata/identity/oauth2/token':
+            # Mock managed identity token endpoint
+            mock_token = {
+                "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjdkRC1nZWNOZ1gxWmY3R0xrT3ZwT0IyZGNWQSIsImtpZCI6IjdkRC1nZWNOZ1gxWmY3R0xrT3ZwT0IyZGNWQSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tLyIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzEyMzQ1Njc4LTkwYWItY2RlZi0xMjM0LTU2Nzg5MGFiY2RlZi8iLCJpYXQiOjE2MjQ5NzQwMDAsIm5iZiI6MTYyNDk3NDAwMCwiZXhwIjoxNjI0OTc3NjAwLCJvaWQiOiIxMjM0NTY3OC05MGFiLWNkZWYtMTIzNC01Njc4OTBhYmNkZWYiLCJzdWIiOiIxMjM0NTY3OC05MGFiLWNkZWYtMTIzNC01Njc4OTBhYmNkZWYiLCJ0aWQiOiIxMjM0NTY3OC05MGFiLWNkZWYtMTIzNC01Njc4OTBhYmNkZWYifQ.fake_signature",
+                "client_id": "12345678-90ab-cdef-1234-567890abcdef",
+                "expires_in": "3600",
+                "expires_on": "1624977600",
+                "ext_expires_in": "3600",
+                "not_before": "1624974000",
+                "resource": "https://management.azure.com/",
+                "token_type": "Bearer"
+            }
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(mock_token).encode())
+        
+        else:
+            self.send_response(404)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            error_response = {"error": "Not Found", "message": f"Endpoint {parsed_url.path} not found"}
+            self.wfile.write(json.dumps(error_response).encode())
---
+++ testinit/testing-server/wireserver_handler.py
@@ -0,0 +1,99 @@
+from http.server import BaseHTTPRequestHandler
+import json
+from urllib.parse import urlparse, parse_qs
+
+from config import WIRESERVER_GET_DELAY, WIRESERVER_GET_TIMEOUT
+from utils import logger
+
+MOCK_WIRESERVER_CONFIG = {
+    "version": "2012-11-30",
+    "goalStateIncarnation": "1",
+    "machine": {
+        "expectedState": "Started",
+        "configurationStatus": "Ready"
+    },
+    "container": {
+        "containerId": "12345678-90ab-cdef-1234-567890abcdef",
+        "roleInstanceList": [
+            {
+                "instanceId": "test-vm",
+                "state": "ReadyRole",
+                "configurationName": "test-config"
+            }
+        ]
+    }
+}
+
+
+class WireServerHandler(BaseHTTPRequestHandler):
+    """HTTP handler for Azure WireServer requests."""
+    self.wireserver_get_delay = WIRESERVER_GET_DELAY
+    self.wireserver_get_timeout = WIRESERVER_GET_TIMEOUT
+
+    def log_message(self, format, *args):
+        """Override to use our logger."""
+        logger.info(f"WireServer: {format % args}")
+    
+    def do_GET(self):
+        """Handle GET requests to WireServer endpoints."""
+        logger.info(f"WireServer GET request: {self.path}")
+        logger.info(f"Headers: {dict(self.headers)}")
+
+        if self.wireserver_get_timeout:
+            logger.info(f"Adding wireserver GET timeout from ENV variable")
+            return
+
+        if self.wireserver_get_delay != 0:
+            logger.info(f"Adding wireserver GET request delay of {self.wireserver_get_delay} seconds")
+            time.sleep(self.wireserver_get_delay)
+        
+        if self.path.startswith('/machine'):
+            # Mock machine configuration endpoint
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/xml')
+            self.end_headers()
+            xml_response = '''<?xml version="1.0" encoding="utf-8"?>
+<GoalState xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="goalstate10.xsd">
+  <Version>2012-11-30</Version>
+  <Incarnation>1</Incarnation>
+  <Machine>
+    <ExpectedState>Started</ExpectedState>
+    <StopRolesDeadlineHint>300000</StopRolesDeadlineHint>
+    <LBProbePorts>
+      <Port>16001</Port>
+    </LBProbePorts>
+  </Machine>
+  <Container>
+    <ContainerId>12345678-90ab-cdef-1234-567890abcdef</ContainerId>
+    <RoleInstanceList>
+      <RoleInstance>
+        <InstanceId>test-vm</InstanceId>
+        <State>ReadyRole</State>
+        <Configuration>
+          <HostingEnvironmentConfig>test-config</HostingEnvironmentConfig>
+        </Configuration>
+      </RoleInstance>
+    </RoleInstanceList>
+  </Container>
+</GoalState>'''
+            self.wfile.write(xml_response.encode())
+        else:
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(MOCK_WIRESERVER_CONFIG).encode())
+    
+    def do_POST(self):
+        """Handle POST requests to WireServer endpoints."""
+        content_length = int(self.headers.get('Content-Length', 0))
+        post_data = self.rfile.read(content_length)
+        
+        logger.info(f"WireServer POST request: {self.path}")
+        logger.info(f"POST data: {post_data.decode('utf-8', errors='ignore')}")
+        
+        # Mock successful response for any POST
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/xml')
+        self.end_headers()
+        response = '<?xml version="1.0" encoding="utf-8"?><Response>OK</Response>'
+        self.wfile.write(response.encode())
