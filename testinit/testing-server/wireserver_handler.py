5a44ed9e6954a91f2dd09ab01de2a25e4aae0002
Cade Jacobson <cadejacobson@microsoft.com>
2025-10-13 09:41:19 -0700

    Use call the custom responses

--- testinit/testing-server/wireserver_handler.py
+++ testinit/testing-server/wireserver_handler.py
@@ -47,7 +47,7 @@ class WireServerHandler(BaseHTTPRequestHandler):
 
             delay_elem = response_elem.find("delay")
             if delay_elem is not None:
-                response_dict["delay"] = delay_elem
+                response_dict["delay"] = int(delay_elem.text)
 
             cls._responses.append(response_dict)
 
@@ -101,9 +101,10 @@ class WireServerHandler(BaseHTTPRequestHandler):
         logger.info(f"WireServer POST request: {self.path}")
         logger.info(f"POST data: {post_data.decode('utf-8', errors='ignore')}")
 
-        # Mock successful response for any POST
-        self.send_response(200)
-        self.send_header("Content-Type", "application/xml")
-        self.end_headers()
-        response = '<?xml version="1.0" encoding="utf-8"?><Response>OK</Response>'
-        self.wfile.write(response.encode())
+        if self._responses is not None:
+            self.write_custom_response()
+            return
+
+        else:
+            self.write_default_response()
+            return
