0d77ffac8622023a6e721416b862ce998f3b6509
Cade Jacobson <cadejacobson@microsoft.com>
2025-10-14 09:47:23 -0700

    Get debug info from the post call

--- libazureinit/src/health.rs
+++ libazureinit/src/health.rs
@@ -192,6 +192,13 @@ async fn _report(
         .timeout(read_timeout)
         .build()?;
 
+    tracing::info!(
+        target: "libazureinit::health::report",
+        "Cade, body={} headers={:?}",
+        body,
+        headers
+    );
+
     let mut remaining = retry_for;
     while !remaining.is_zero() {
         let (resp, new_remaining) = http::post(
