00af9b32bf46a1b7938cae301a59cd44ffe2c269
Cade Jacobson <cadejacobson@microsoft.com>
2025-10-16 10:47:48 -0700

    Mount to a place that already exists

--- testinit/start-all.sh
+++ testinit/start-all.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo mkdir -p /var/lib/hyperv
+sudo touch /var/lib/hyperv/.kvp_pool_1
 
 echo "Starting testing-server first (creates networks with Azure IP addresses)..."
 pushd testing-server
