1176abde2099d22d8d687415ffef43337decce14
Cade Jacobson <cadejacobson@microsoft.com>
2026-04-21 11:42:11 -0700

    Test codecov

--- libazureinit/src/http.rs
+++ libazureinit/src/http.rs
@@ -118,6 +118,7 @@ async fn request(
                         let statuscode = response.status();
                         span.record("http_status", statuscode.as_u16());
                         tracing::info!(target: "libazureinit::http::received", url=response.url().as_str(), "HTTP response received");
+                        tracing::info!(target: "libazureinit::http::received", url=response.url().as_str(), "Cade test for codecov.");
 
                         match response.error_for_status() {
                             Ok(response) => {
