5c463d5ed4d320127ad688b3ab9a174cb8317c5e
Cade Jacobson <cadejacobson@microsoft.com>
2026-05-05 08:36:53 -0700

    Revert intentional time out failure

--- testinit/start-all.sh
+++ testinit/start-all.sh
@@ -22,7 +22,7 @@ while true; do
   # Check journal logs for either a success or a failure to ensure the completion of the service
   if docker exec azureinit-provisioning-agent journalctl -u azure-init.service --no-pager | grep -q "Finished azure-init.service"; then
     echo "azure-init.service has finished"
-    # break
+    break
   fi
   if  docker exec azureinit-provisioning-agent journalctl -u azure-init.service --no-pager | grep -i 'azure-init' | grep -Eiq 'failed|failure'; then
     echo "azure-init.service has failed."
